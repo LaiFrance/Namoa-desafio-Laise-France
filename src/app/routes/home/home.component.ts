cb8c84a14f27bfb93574c4157089211df8db0970
Laise France <laise94@hotmail.com>
2023-06-13 12:26:43 -0300

    ajuste estilo dos cards

--- src/app/routes/home/home.component.ts
+++ src/app/routes/home/home.component.ts
@@ -3,29 +3,29 @@ import { ProductService } from 'src/app/product-service.service';
 import { APIProductsRoot } from 'src/app/api-services/products.service.interface';
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+	selector: 'app-home',
+	templateUrl: './home.component.html',
+	styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
-  smartphones: APIProductsRoot;
-  laptops: APIProductsRoot;
+	smartphones: APIProductsRoot;
+	laptops: APIProductsRoot;
 
-  constructor(private productService: ProductService) { }
+	constructor(private productService: ProductService) { }
 
-  ngOnInit(): void {
-    this.productService.getProductByType('smartphones').subscribe((data) => {
-      this.smartphones = data;
-      this.smartphones.products.forEach((item) => {
-        item.rating = Math.round(item.rating);
-      });
-    });
+	ngOnInit(): void {
+		this.productService.getProductByType('smartphones').subscribe((data) => {
+			this.smartphones = data;
+			this.smartphones.products.forEach((item) => {
+				item.rating = Math.round(item.rating);
+			});
+		});
 
-    this.productService.getProductByType('laptops').subscribe((data) => {
-      this.laptops = data;
-      this.laptops.products.forEach((item) => {
-        item.rating = Math.round(item.rating);
-      });
-    });
-  }
+		this.productService.getProductByType('laptops').subscribe((data) => {
+			this.laptops = data;
+			this.laptops.products.forEach((item) => {
+				item.rating = Math.round(item.rating);
+			});
+		});
+	}
 }
